1ab32b97338f8f0f2648b47d49bfd86c931067dc
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-11-25 17:47:22 +0100

    add Dictionary and Lemma models

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from pydantic import BaseModel
+
+
+class Dictionary(Enum):
+    BWB = "bwb"
+    WBF = "wbf"
+    DIBS = "dibs"
+
+
+class LemmaItem(BaseModel):
+    source: Dictionary
+    lemma: str
+    pos: str
+
+
+class LemmaResult(BaseModel):
+    results: list[LemmaItem]
