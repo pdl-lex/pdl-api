56122bbb2c76f18cc45dfc95617fcc317bbfba4a
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-03 13:52:51 +0100

    remove api prefix

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ from app.services.lemma_service import LemmaService
 app = FastAPI()
 
 
-@app.get("/api/lemma/{lemma}")
+@app.get("/lemma/{lemma}")
 def get_lemma(lemma: str, lemma_service: LemmaService = Depends(LemmaService)):
     return lemma_service.query_lemma(lemma)
