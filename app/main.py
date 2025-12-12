ee41546df23448bc58600f14776f7e324d5c79cd
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-12 18:32:32 +0100

    use lifespan logic for lemma service

--- app/main.py
+++ app/main.py
@@ -1,20 +1,27 @@
-from fastapi import Depends, FastAPI
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
 
 from app.models import Entry
 from app.services.lemma_service import LemmaService
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    lemma_service = LemmaService()
+    app.state.lemma_service = lemma_service
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/lemma/{lemma_id}")
-def fetch_lemma_entry(
-    lemma_id: str, lemma_service: LemmaService = Depends(LemmaService)
-) -> Entry:
-    return lemma_service.fetch_lemma(lemma_id)
+def fetch_lemma_entry(lemma_id: str) -> Entry:
+    return app.state.lemma_service.fetch_lemma(lemma_id)
 
 
 @app.get("/search")
-def free_text_search(
-    q: str = "Suchwort", lemma_service: LemmaService = Depends(LemmaService)
-) -> list[Entry]:
-    return lemma_service.free_text_search(q)
+def free_text_search(q: str = "Suchwort") -> list[Entry]:
+    return app.state.lemma_service.free_text_search(q)
