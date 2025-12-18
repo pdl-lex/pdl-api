2952847758c106a038caf227afdebcd5249872f4
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-18 19:33:32 +0100

    add free text search

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import Depends, FastAPI
 
+from app.models import Entry
 from app.services.lemma_service import LemmaService
 
 app = FastAPI()
 
 
-@app.get("/lemma/{lemma}")
-def get_lemma(lemma: str, lemma_service: LemmaService = Depends(LemmaService)):
-    return lemma_service.query_lemma(lemma)
+@app.get("/lemma/{lemma_id}")
+def fetch_lemma_entry(
+    lemma_id: str, lemma_service: LemmaService = Depends(LemmaService)
+) -> Entry:
+    return lemma_service.fetch_lemma(lemma_id)
+
+
+@app.get("/search")
+def free_text_search(
+    q: str = "Suchwort", lemma_service: LemmaService = Depends(LemmaService)
+) -> list[Entry]:
+    return lemma_service.free_text_search(q)
--- app/services/lemma_service.py
+++ app/services/lemma_service.py
@@ -1,15 +1,25 @@
 import os
 
+from fastapi import HTTPException
 from pymongo import MongoClient
+
+from app.models import Entry
 
 
 class LemmaService:
     def __init__(self):
         self.client = MongoClient(os.environ["MONGODB_URI"])
-        self.db = self.client["wb"]
-        self.collection = self.db["bwb"]
+        self.db = self.client["lex"]
+        self.collection = self.db["entries"]
 
-    def query_lemma(self, lemma: str) -> dict:
-        result = self.collection.find_one({"entry.form.orth": lemma})
+    def free_text_search(self, query: str) -> list[Entry]:
+        results = self.collection.find({"$text": {"$search": query}})
 
-        return result.get("entry") if result else {"error": "Lemma not found"}
+        return [result.get("entry") for result in results]
+
+    def fetch_lemma(self, lemma_id: str) -> Entry:
+        result = self.collection.find_one({"entry.xml:id": lemma_id})
+        if result is None:
+            raise HTTPException(status_code=404, detail=f"Unknown id: {lemma_id!r}")
+
+        return result["entry"]
