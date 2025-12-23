a97cc6bc67fae8fcaabfcd7b6f0dc14fe2b98107
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2025-12-23 18:00:57 +0100

    add index creation

--- app/services/import_service.py
+++ app/services/import_service.py
@@ -1,11 +1,19 @@
 import os
 
 from pydantic import TypeAdapter
-from pymongo import MongoClient
+from pymongo import ASCENDING, IndexModel, MongoClient
 
 from app.models import DisplayEntry
 
 fulltext_search_fields = ["headword", "flatSenses.def"]
+index_fields = [
+    "source",
+    "xml:id",
+    "headword",
+    "pos",
+    "gender",
+    "number",
+]
 
 
 class ImportService:
@@ -18,6 +26,15 @@ class ImportService:
         self.display.delete_many({})
         self.display.drop_indexes()
 
+    def _create_indexes(self):
+        self.display.create_index(
+            [(field, "text") for field in fulltext_search_fields], name="fulltextIndex"
+        )
+
+        self.display.create_indexes(
+            [IndexModel([(field, ASCENDING)]) for field in index_fields]
+        )
+
     def insert_display_data(self, data: list[DisplayEntry]):
         self._reset_display_collection()
 
@@ -26,6 +43,4 @@ class ImportService:
 
         self.display.insert_many(dump)
 
-        self.display.create_index(
-            [(field, "text") for field in fulltext_search_fields], name="fulltextIndex"
-        )
+        self._create_indexes()
