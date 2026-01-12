7d147568d9ca005c0890cdf776f54510580b78ed
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-01-12 21:24:29 +0100

    include etymology in free text search

--- app/services/import_service.py
+++ app/services/import_service.py
@@ -9,6 +9,7 @@ fulltext_search_fields = [
     {"key": "headword.lemma", "weight": 10},
     {"key": "flatSenses.def", "weight": 1},
     {"key": "flatSenses.cit.quote", "weight": 1},
+    {"key": "etym", "weight": 1},
 ]
 index_fields = [
     "source",
