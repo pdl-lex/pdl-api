bfc5f2918272147a29982478004442259fbb4041
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-02-02 17:14:50 +0100

    add data transformation wrapper script

---
+++ app/transformers/lex_transformer.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from tqdm import tqdm
+
+from app.transformers.bdo.bdo_transformer import BdoXmlTransformer
+
+OUTPUT_DATA_DIR = Path("data/display_data")
+OUTPUT_ERROR_DIR = Path("data/display_data")
+
+
+def ensure_output_directories():
+    OUTPUT_DATA_DIR.mkdir(parents=True, exist_ok=True)
+    OUTPUT_ERROR_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def bdo_to_lexoterm(bdo_dir: Path) -> list[dict]:
+    files = list(bdo_dir.rglob("*.xml"))
+    result = []
+
+    progress_bar = tqdm(files, desc="Converting BDO XML to LexoTerm format")
+
+    for path in progress_bar:
+        progress_bar.set_description(f"Processing {path.parent.parent.name}")
+        transformer = BdoXmlTransformer(path)
+        result.append(transformer.transform())
+
+    return result
+
+
+def dwds_to_lexoterm(dwds_dir: Path):
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser(
+        description="Convert lexical resources to LexoTerm JSON"
+    )
+    parser.add_argument("resource", help="Resource name", choices=["bdo", "dwds"])
+    parser.add_argument(
+        "-p", "--path", help="Path to source folder", required=True, type=Path
+    )
+
+    args = parser.parse_args()
+
+    dispatch = {
+        "bdo": bdo_to_lexoterm,
+        "dwds": dwds_to_lexoterm,
+    }
+
+    result = dispatch[args.resource](args.path)
+
+    ensure_output_directories()
+
+    output_path = OUTPUT_DATA_DIR / f"{args.resource}.json"
+
+    with open(output_path, "w") as json_file:
+        json.dump(result, json_file, ensure_ascii=False, indent=2)
+
+    print(f"Stored data in {output_path}")
