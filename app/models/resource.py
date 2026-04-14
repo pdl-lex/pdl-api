779650e65a82b9f1ff3ae5ed3f6270aa6f1de2a7
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-04-14 16:21:05 +0200

    add Resource

--- app/models/resource.py
+++ app/models/resource.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from typing import Optional
+
+from app.models.base import BaseModel
 
 
 class ResourceName(Enum):
@@ -7,3 +10,9 @@ class ResourceName(Enum):
     DIBS = "dibs"
     WBF = "wbf"
     DWDS = "dwds"
+
+
+class Resource(BaseModel):
+    name: ResourceName
+    credits: Optional[str] = None
+    license: Optional[str] = None
