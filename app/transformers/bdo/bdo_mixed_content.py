144ae792a100ed62e4a6da205f813aae7ca53dd6
Fabian Simonjetz <fabian.simonjetz@adl.badw.de>
2026-04-29 16:17:58 +0200

    Revert "swap arguments"
    This reverts commit ec94e5551ca46bda424cf92b6f65b4b559fa9d07.

--- app/transformers/bdo/bdo_mixed_content.py
+++ app/transformers/bdo/bdo_mixed_content.py
@@ -58,7 +58,7 @@ class BdoBaseTransformer(StandoffTransformer):
 
         return (
             aframe.assign(ref_type=mapped_ref_types)
-            .insert_attribute("ref_type", "verweis")
+            .insert_attribute("verweis", "ref_type")
             .drop("ref_type", axis=1)
         )
 
@@ -96,7 +96,7 @@ class BdoBaseTransformer(StandoffTransformer):
 class BdoLiteratureTransformer(StandoffTransformer):
     @preprocess(order=1)
     def insert_literature_prefixes(self, aframe: AnnotationFrame) -> AnnotationFrame:
-        return aframe.insert_attribute("quelle-art", "literatur-quelle")
+        return aframe.insert_attribute("literatur-quelle", "quelle-art")
 
     @preprocess(order=3)
     def add_bib_id_column(self, aframe: AnnotationFrame) -> AnnotationFrame:
